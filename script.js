9cb403e1c63dbf808f17d327413bc556ae99cc18
Elias Calagiu <eliasc81@gmail.com>
2020-12-18 11:17:51 -0800

    AJAX retrieving data and logging in console

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 // Url Ajax will be using to retrieve information
 
-var QueryUrl = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=query&fq=filter&api-key=32GLA4k3GlzsT62GQc4kXW8LUxUJrs5x"
+var QueryUrl = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=nfl&fq=seahawks&api-key=32GLA4k3GlzsT62GQc4kXW8LUxUJrs5x"
+
+$.ajax({
+    url: QueryUrl,
+    method: "GET"
+}).then(function(response){
+    console.log(response);
+    // for(var i=0; i<response.docs.length; i++){
+    //     var randomDocs = $("<p>");
+    //     randomDocs.text(response.docs[i]);
+    //     $("body").append(randomDocs);
+    // }
+    // var randomDocs = $("<p>");
+    // randomDocs.text(response.docs[i]);
+    // $("body").append(randomDocs);
+})
